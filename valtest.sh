b58cd8ac13751c5389e59a61a94c2a7e9a5dfa7d
Eric Chen <eric.chen.21@dartmouth.edu>
2020-10-06 12:26:40 -0400

    final commit

--- valtest.sh
+++ valtest.sh
@@ -6,9 +6,5 @@ valgrind --leak-check=full testGet
 printf "\n\n\n\n"
 valgrind --leak-check=full testApply
 printf "\n\n\n\n"
-<<<<<<< HEAD
-valgrind --leak-check=full testRemove
-=======
 valgrind --leak-check=full testRemove
 printf "\n\n\n\n"
->>>>>>> 9bfdde1b544839566080d017a92d492872b49dbd
