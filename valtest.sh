0b084dee9f1eed9b5a9e0875b256bdf256083e8b
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-10-06 23:29:04 -0400

    updated valtest.sh to only show Error Summary

--- valtest.sh
+++ valtest.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 touch memcheck.txt
 valgrind --leak-check=full testPut &>> memcheck.txt
 valgrind --leak-check=full testGet &>> memcheck.txt
@@ -9,13 +8,3 @@ valgrind --leak-check=full testRemove &>> memcheck.txt
 
 grep "ERROR SUMMARY\| Command: " memcheck.txt
 rm memcheck.txt
-=======
-valgrind --leak-check=full testPut 
-printf "\n\n\n\n"
-valgrind --leak-check=full testGet
-printf "\n\n\n\n"
-valgrind --leak-check=full testApply
-printf "\n\n\n\n"
-valgrind --leak-check=full testRemove
-printf "\n\n\n\n"
->>>>>>> 767a730a01a8acbc43be5072cdc07a9378d9f553
