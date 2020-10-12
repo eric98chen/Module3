ba3c614c5724bb5549a54479f5dd6d7bfeffb43e
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-10-12 01:41:42 -0400

    Improved valtest.sh

--- Queue/valtest.sh
+++ Queue/valtest.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+echo making object files and executables...
+make &>> /dev/null
 
-touch memcheck.txt
+echo running valgrind...
 valgrind --leak-check=full chrisTest &>> memcheck.txt
+grep "ERROR SUMMARY\| Command: " memcheck.txt; > memcheck.txt
 valgrind --leak-check=full test1to4 &>> memcheck.txt
+grep "ERROR SUMMARY\| Command: " memcheck.txt; > memcheck.txt
 valgrind --leak-check=full testQapply &>> memcheck.txt
+grep "ERROR SUMMARY\| Command: " memcheck.txt; > memcheck.txt
 
-grep "ERROR SUMMARY\| Command: " memcheck.txt
 rm memcheck.txt
