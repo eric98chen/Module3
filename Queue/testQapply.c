972b7698d20d48c6e1d4bbf1131c7c6c14788791
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-10-12 00:14:28 -0400

    Added test for qapply() in queue.c

---
+++ Queue/testQapply.c
@@ -0,0 +1,84 @@
+/* testQapply.c -- test qapply() in queue.c
+ *
+ * Author: David Domonoske
+ * Created: Sun Oct 11
+ * Version:
+ *
+ * Description: returns 0 if success, nonzero if fail
+ *
+ */
+ 
+#include <stdio.h>
+#include "queue.h"
+#include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+#define MAXREG 10
+
+
+/* the representation of a car */
+typedef struct car {
+	char plate[MAXREG];
+	double price;
+	int year;
+} car_t;
+
+
+/* makeCar(): creates a car object */
+car_t* make_car(char *platep, double price, int year) {
+	car_t *cp;
+	if ((cp = (car_t*)malloc(sizeof(car_t))) == NULL) { 	//check if enough space in memory for car_t*
+		printf("Error: malloc failed allocating car\n");
+		return NULL;
+	}
+	printf("car malloc succesful\n");
+	strcpy(cp->plate, platep);
+	cp->price = price;
+	cp->year = year;
+	return cp;
+}
+
+
+/* fn() takes a car_t pointer and sets year of car_t to 0
+ * returns nothing
+ */
+void fn(void *p) {
+	car_t *cp = (car_t*)p;
+	cp->year = 0;
+}
+	
+
+int main (void) {
+	queue_t *qp; //agnostic queue
+	car_t *cp1;
+	car_t *cp2;
+	car_t *cp3;
+	int32_t result;
+	
+	qp = qopen(); //returns pointer to newly created queue_t
+
+	cp1 = make_car("car1", 1.0, 1910); //declare all cars
+	cp2 = make_car("car2", 4.0, 1940);
+	cp3 = make_car("car3", 9.0, 1990);
+
+	// test with NULLs
+	qapply(NULL, NULL);
+	qapply(NULL, &fn);
+	qapply((queue_t*)qp, NULL);
+
+	result = 0;
+	result += qput(qp, (void*)cp1);
+	result += qput(qp, (void*)cp2);
+	result += qput(qp, (void*)cp3);
+
+	if ( result > 0 ) {
+		fprintf(stderr, "Error while adding cars to queue\n");
+		exit(EXIT_FAILURE);
+	}
+	 
+	qclose(qp);
+	
+	exit(EXIT_SUCCESS);
+}
+
