5b378d3b7b0f4b107ca6603575dd68af72f36d44
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-10-12 01:10:51 -0400

    Added testQapply to valtest.sh

--- Queue/valtest.sh
+++ Queue/valtest.sh
@@ -3,6 +3,7 @@
 touch memcheck.txt
 valgrind --leak-check=full chrisTest &>> memcheck.txt
 valgrind --leak-check=full test1to4 &>> memcheck.txt
+valgrind --leak-check=full testQapply &>> memcheck.txt
 
 grep "ERROR SUMMARY\| Command: " memcheck.txt
 rm memcheck.txt
