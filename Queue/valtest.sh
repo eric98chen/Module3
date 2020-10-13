e9cc6e28b5f944749fe7875b606f8762d584ee7f
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-10-13 14:23:42 -0400

    Updated valtest.sh in Queue/

--- Queue/valtest.sh
+++ Queue/valtest.sh
@@ -3,18 +3,18 @@ echo making object files and executables...
 make &>> /dev/null
 
 echo running valgrind...
-valgrind --leak-check=full chrisTest &>> memcheck.txt
-grep "ERROR SUMMARY\| Command: " memcheck.txt; > memcheck.txt
-valgrind --leak-check=full test1to4 &>> memcheck.txt
-grep "ERROR SUMMARY\| Command: " memcheck.txt; > memcheck.txt
-valgrind --leak-check=full testQapply &>> memcheck.txt
-grep "ERROR SUMMARY\| Command: " memcheck.txt; > memcheck.txt
-valgrind --leak-check=full testQsearch &>> memcheck.txt
-grep "ERROR SUMMARY\| Command: " memcheck.txt; > memcheck.txt
-valgrind --leak-check=full testQremove &>> memcheck.txt
-grep "ERROR SUMMARY\| Command: " memcheck.txt; > memcheck.txt
-valgrind --leak-check=full testQconcat &>> memcheck.txt
-grep "ERROR SUMMARY\| Command: " memcheck.txt; > memcheck.txt
+valgrind --leak-check=full chrisTest &> memcheck.txt
+grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
+valgrind --leak-check=full test1to4 &> memcheck.txt
+grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
+valgrind --leak-check=full testQapply &> memcheck.txt
+grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
+valgrind --leak-check=full testQsearch &> memcheck.txt
+grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
+valgrind --leak-check=full testQremove &> memcheck.txt
+grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
+valgrind --leak-check=full testQconcat &> memcheck.txt
+grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
 
 rm memcheck.txt
 echo making clean...
